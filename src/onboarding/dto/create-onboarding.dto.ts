274c7883424a09eec3f53f14a0756c762837f1e6
Jeison Rojas <j_esteban96@hotmail.com>
2026-01-16 13:15:42 -0500

    Endpoints documented with Swagger

--- src/onboarding/dto/create-onboarding.dto.ts
+++ src/onboarding/dto/create-onboarding.dto.ts
@@ -6,19 +6,24 @@ import {
   IsString,
 } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOnboardingDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   document: string;
 
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ minimum: 0 })
   @Type(() => Number)
   @IsNumber()
   @Min(0)
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class Product {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
-
+  
+  @ApiProperty()
   @Column()
   name: string;
-
+  
+  @ApiProperty()
   @Column()
   description: string;
-
+  
+  @ApiProperty()
   @Column('decimal')
   price: number;
 }
