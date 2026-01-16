2cbfc46b63d015df1de8b42b6e4fc212a0541e23
Jeison Rojas <j_esteban96@hotmail.com>
2026-01-16 12:15:07 -0500

    Onboarding services done

--- src/onboarding/dto/create-onboarding.dto.ts
+++ src/onboarding/dto/create-onboarding.dto.ts
@@ -1,1 +1,26 @@
-export class CreateOnboardingDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  Min,
+  IsString,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateOnboardingDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  document: string;
+
+  @IsEmail()
+  email: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  @Min(0)
+  initialAmount: number;
+}
