a59d55a2f27c6e5a5929fc4623111b1516c63fa1
Jeison Rojas <j_esteban96@hotmail.com>
2026-01-16 11:55:32 -0500

    Create products services and implement redis cache

--- src/onboarding/onboarding.module.ts
+++ src/onboarding/onboarding.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { OnboardingService } from './onboarding.service';
 import { OnboardingController } from './onboarding.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Onboarding } from './entities/onboarding.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Onboarding,
+    ])
+  ],
   controllers: [OnboardingController],
   providers: [OnboardingService],
 })
-export class OnboardingModule {}
+export class OnboardingModule { }
